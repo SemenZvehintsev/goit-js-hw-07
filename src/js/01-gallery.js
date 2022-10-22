8958b602c5b4488187a22af5afb00e2d06d8e049
SemenZvehintsev <kvadratniibomzh@gmail.com>
2022-10-22 19:22:56 +0300

    added first task

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,4 +1,52 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+class Gallery {
+    constructor({preview, original, description}) {
+        this.preview = preview;
+        this.original = original;
+        this.description = description;
+        this.fullGallery = document.querySelector('.gallery');
+    }
+
+    start() {
+        this.createGallery()
+        this.openFullImg()
+    }
+
+    createGallery() {
+        galleryItems.forEach(({preview, original, description}) => {
+        this.fullGallery.insertAdjacentHTML("afterbegin", 
+        `<div class="gallery__item">
+        <a class="gallery__link" href="${original}">
+        <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+        />
+        </a>
+        </div>`);})
+    }
+
+    openFullImg() {
+        this.fullGallery.addEventListener('click', this.fullImg)
+    }
+    
+    fullImg(event) {
+        event.preventDefault();
+        const srcImg = Object.values(event.target.dataset);
+        const fullImg = basicLightbox.create(`
+            <img width="1280" src="${srcImg}">
+        `);
+        fullImg.show()
+
+        window.addEventListener('keydown', function closeByEsc(event) {
+            if (event.code === 'Escape') {
+            fullImg.close()
+            window.removeEventListener('keydown', closeByEsc)
+            }
+        });
+    }    
+}
+
+new Gallery(galleryItems).start();
